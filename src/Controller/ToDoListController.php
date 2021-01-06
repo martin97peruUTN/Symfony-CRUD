fcd5f8fd914eff981e2a3bc61790c8b1a8ff2c66
Martin Perussini <mperussini@frsf.utn.edu.ar>
2021-01-06 15:31:26 -0300

    Finished

--- src/Controller/ToDoListController.php
+++ src/Controller/ToDoListController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,15 +15,27 @@ class ToDoListController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render("index.html.twig");
+        $tasks = $this->getDoctrine()->getRepository(Task::class)->findBy([],["id"=>"DESC"]);
+        return $this->render("index.html.twig",["tasksitas"=>$tasks]);
     }
 
     /**
      * @Route("/create", name="create_task", methods={"POST"})
+     * @param Request $request
+     * @return Response
      */
-    public function create(): Response
+    public function create(Request $request): Response
     {
-        exit("to do: create a new task");
+        $title=trim($request->request->get("title"));
+        if(empty($title)){
+            return $this->redirectToRoute("to_do_list");
+        }
+        $entityManager = $this->getDoctrine()->getManager();
+        $task = new Task();
+        $task->setTitle($title);
+        $entityManager->persist($task);
+        $entityManager->flush();
+        return $this->redirectToRoute("to_do_list");
     }
 
     /**
@@ -29,7 +43,12 @@ class ToDoListController extends AbstractController
      */
     public function switchStatus($id): Response
     {
-        exit("SwitchStatus".$id);
+        $em = $this->getDoctrine()->getManager();
+        $task = $em->getRepository(Task::class)->find($id);
+        $task->setStatus(!$task->getStatus());
+        $em->persist($task);
+        $em->flush();
+        return $this->redirectToRoute("to_do_list");
     }
 
     /**
@@ -37,6 +56,10 @@ class ToDoListController extends AbstractController
      */
     public function delete($id): Response
     {
-        exit("delete ".$id);
+        $em = $this->getDoctrine()->getManager();
+        $task = $em->getRepository(Task::class)->find($id);
+        $em->remove($task);
+        $em->flush();
+        return $this->redirectToRoute("to_do_list");
     }
 }
